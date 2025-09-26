d1d43059578ac46432bd155521b04cde0eacea1a
talentestors <talentestors@gmail.com>
2025-09-26 16:37:24 +0800

    feat: ✨ add gitcommit parser

--- lua/conf/nvim-treesitter.lua
+++ lua/conf/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "vim", "vimdoc", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+    ensure_installed = { "gitcommit", "vim", "vimdoc", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
@@ -22,7 +22,6 @@ require 'nvim-treesitter.configs'.setup {
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
         -- the name of the parser)
         -- list of language that will be disabled
-        disable = {},
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         disable = function(lang, buf)
             local max_filesize = 100 * 1024 -- 100 KB
